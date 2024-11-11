14551f1a7e2588d0b8a0a8dc138482b2e5e70e8d
gytks4 <gytks4@naver.com>
2024-11-11 14:20:44 +0900

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { RegistrationForm } from "./enrollment/RegistrationForm";
+import { RegistrationForm } from "./registration/registration-form";
 
 function App() {
   return (
