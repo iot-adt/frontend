40f9c4e8731d393786adc30ff80edcec4d69dff4
gytks4 <gytks4@naver.com>
2024-11-21 21:39:38 +0900

    feat: style

--- src/cctv/video-fallback.tsx
+++ src/cctv/video-fallback.tsx
@@ -2,7 +2,7 @@ import { VideoOff } from "lucide-react";
 
 export default function VideoFallback() {
   return (
-    <div className="text-gray-400 flex flex-col items-center">
+    <div className="text-gray-400 flex flex-col justify-center items-center w-full h-full">
       <VideoOff className="h-12 w-12 mb-2" />
       <span>영상 없음</span>
     </div>
