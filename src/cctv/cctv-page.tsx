01e12d8f9b0d79918e3041e02c7c509b2cd6b380
gytks4 <gytks4@naver.com>
2024-11-25 13:45:34 +0900

    feat: url 수정

--- src/cctv/cctv-page.tsx
+++ src/cctv/cctv-page.tsx
@@ -12,7 +12,7 @@ type CCTVFeed = {
 };
 
 const cctvFeeds: CCTVFeed[] = [
-  { id: "1", name: "출입문", url: "ws://10.147.20.102:5000/video" },
+  { id: "1", name: "출입문", url: "ws://192.168.1.165:5000/video" },
   { id: "2", name: "1층 내부", url: null },
   { id: "3", name: "2층 내부", url: null },
   { id: "4", name: "외부", url: null },
