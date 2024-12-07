b30d4d009aa439381f9b70d66c5f2292bc1c8736
gytks4 <gytks4@naver.com>
2024-12-07 17:41:52 +0900

    feat: api path

--- src/remote/index.ts
+++ src/remote/index.ts
@@ -4,7 +4,8 @@ const window = axios.create({
   baseURL: "http://10.144.115.93:5001",
 });
 const api = axios.create({
-  baseURL: "http://10.144.85.43:8080/api",
+  // baseURL: "http://10.144.85.43:8080/api",
+  baseURL: "http://10.144.45.196:8080/api",
 });
 
 export const CLIENT = {
